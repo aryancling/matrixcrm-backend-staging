c0d22d739616bc3a658744cce2af3b6771128b27
aryanvividweb <aryan@vividwebtechnologies.com>
2024-12-24 16:57:00 +0530

    changed DB URL

--- config/db.js
+++ config/db.js
@@ -3,14 +3,9 @@ const logger = require("./logger.config");
 require("dotenv").config();
 
 mongoose
-  .connect(
-    `mongodb://${process.env.DATABASE_USERNAME}:${encodeURIComponent(
-      process.env.DATABASE_PASSWORD
-    )}@${process.env.DATABASE_URL}`,
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
   .then(() => {
     logger.info("✅ MongoDB connected successfully");
   })
