647c69d44cb18756aec692e0dfb6395569bb0a5c
aryanvividweb <aryan@vividwebtechnologies.com>
2025-05-28 22:09:24 +0530

    fixed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const router = require("./router");
 const { errorHandler } = require("./middleware/errorHandler");
 require("./config/db");
@@ -11,18 +10,10 @@ const app = express();
 app.use(express.static("public"));
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(express.json({ limit: "100mb" }));
-
-// middleware
-// app.use((req, res, next) => {
-//   // auth middleware will be here
-//   next();
-// });
-
 app.use("/api/v1", router);
 
-// error handler
 app.use((error, req, res, next) => {
   errorHandler(error, req, res, next);
 });
